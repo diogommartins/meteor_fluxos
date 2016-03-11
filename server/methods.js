ba5a992775c6a6077570f1087afcab5fa9f5f922
diogommartins <diogo.martins@unirio.br>
2016-03-10 23:49:59 -0300

    Refatoração

---
+++ server/methods.js
@@ -0,0 +1,11 @@
+/**
+ * Created by diogomartins on 3/10/16.
+ */
+Meteor.methods({
+    getFluxo: function(id_tipo_doc){
+        const resource = "http://sistemas.unirio.br/api/FLUXOS?FORMAT=JSON&FIELDS=DESCR_FLUXO%2CSITUACAO_ATUAL%2CSITUACAO_FUTURA%2CTIPO_DESTINO%2CID_DESTINO%2CID_FLUXO&LMAX=100&ID_TIPO_DOC="+id_tipo_doc+"&IND_ATIVO=S&API_KEY=9287c7e89bc83bbce8f9a28e7d448fa7366ce23f163d2c385966464242e0b387e3a34d0e205cb775d769a44047995075&LMIN=1";
+        console.log("Consultando: " + resource);
+        return HTTP.get(resource).data.content;
+
+    }
+});
