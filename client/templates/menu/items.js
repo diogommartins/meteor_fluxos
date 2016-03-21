66e68d897a84897c171a2ff94bf67981819f730c
diogommartins <diogo.martins@unirio.br>
2016-03-21 19:28:41 -0300

    Refatoração

--- client/templates/menu/items.js
+++ client/templates/menu/items.js
@@ -2,12 +2,15 @@
  * Created by diogomartins on 3/16/16.
  */
 Template.menuItem.helpers({
-
+    node: function(){
+        return window.graph.nodesMenu.currentItem;
+    }
 });
 
 Template.menuItem.events({
     'click a': function(event, template){
+        const node = window.graph.nodesMenu.currentItem;
         const menuItem = template.data;
-        menuItem.action();
+        menuItem.action(node);
     }
 });
