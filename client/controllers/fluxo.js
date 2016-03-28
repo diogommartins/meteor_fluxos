ec9aca46a9d670556099f69087a4f176749266b5
diogommartins <diogo.martins@unirio.br>
2016-03-28 19:43:04 -0300

    Mudança de layout funcional !!!!!

--- client/controllers/fluxo.js
+++ client/controllers/fluxo.js
@@ -2,16 +2,29 @@
 Template.graphContainer.created = function(){
     const id_tipo_doc = this.data.id_tipo_doc;
 
-    //let cyGraph = CyGraphs.findOne({id_tipo_doc: id_tipo_doc});
-
     Meteor.call('getFluxo', id_tipo_doc, function(error, elements){
-        var graph = new Graph(id_tipo_doc).renderGraph();
+        const graph = new Graph(id_tipo_doc).renderGraph();
 
         graph.cy.ready(function () {
             var cy = this;
-            graph.load(elements).applyStyle('circle');
+            var cyGraph = CyGraphs.find({id_tipo_doc: id_tipo_doc});
 
-            var nodesObserver = Nodes.find({id_tipo_doc: id_tipo_doc}).observeChanges({
+            graph.load(elements).applyStyle(cyGraph.fetch()[0].layout);
+
+            const nodes = Nodes.find({id_tipo_doc: id_tipo_doc});
+
+            var cyObserver = cyGraph.observeChanges({
+                changed: function(_id, data){
+                    if (typeof data.layout !== 'undefined' && data.layout !== 'preset') {
+                        // graph.changeLayout(data.layout);
+                        var layout = graph.cy.makeLayout({name: data.layout});
+                        layout.run();
+                        ;
+                    }
+                }
+            });
+
+            var nodesObserver = nodes.observeChanges({
                 added: function (_id, newNode) {
                     if (!nodesObserver) return;
                     newNode._id = _id;
@@ -20,7 +33,13 @@ Template.graphContainer.created = function(){
                 changed: function (nodeId, data) {
                     const node = graph.getNodeById(nodeId);
                     if (typeof data.position !== 'undefined'){
-                        graph.cy.$("#"+node.data.id).position(data.position);
+                        if (!graph.isGrabbed(node)) {
+                            const currentPosition = graph.cy.$("#" + node.data.id).position();
+                            if ((currentPosition.x !== data.position.x) ||
+                                (currentPosition.y !== data.position.y)){
+                                graph.cy.$("#" + node.data.id).position(data.position);
+                            }
+                        }
                     }
                     else if (typeof data.data === 'object'){
                         const updatedNodeData = data.data;
@@ -29,7 +48,7 @@ Template.graphContainer.created = function(){
                     }
                 },
                 removed: function (nodeId) {
-                    console.log("Removed");
+                    console.log("Removed " + nodeId);
                     let node = graph.getNodeById(nodeId);
                     graph.removeElement('nodes', node);
                     graph.refresh();
@@ -68,14 +87,19 @@ Template.graphContainer.created = function(){
                 let target = event.cyTarget;
                 if (target.isNode()){
                     const node = graph.getNodeByData(target.data());
-                    
+
                     let position = this.$("#"+node.data.id).position();
 
                     if (typeof graph.visibleMenu !== 'undefined')
                         graph.visibleMenu.updatePosition(position);
 
-                    Nodes.update({_id: node._id}, { $set: {position: position}});
-                    // Meteor.call('updateNodePosition', node, position);
+                    const currentCyGraph = cyGraph.fetch()[0];
+                    const layout = currentCyGraph.layout;
+                    if (typeof layout === 'undefined' || layout !== 'preset'){
+                        CyGraphs.update({_id:currentCyGraph._id}, {$set:{layout:'preset'}});
+                        console.log("Atualizei layout");
+                    }
+                    Meteor.call('updateNodeData', node, {position: position});
                 }
             });
             cy.on('pan', function (event) {
@@ -88,6 +112,13 @@ Template.graphContainer.created = function(){
                         y: graph.visibleMenu.position.y * zoom + pan.y
                     });
                 }
+            });
+            cy.on('layoutstop', function(event){
+                const elements = event.layout.options.eles.nodes();
+                elements.map(function(element){
+                    const node = graph.getNodeByData(element.data());
+                    Meteor.call('updateNodeData', node, {position: node.position});
+                });
             });
             cy.on('zoom', function (event) {
                 const zoom = event.cyTarget.zoom();
@@ -102,16 +133,6 @@ Template.graphContainer.created = function(){
 };
 
 
-
-Template.graphContainer.events({
-    'nodeDidChange': function(event, template){
-        console.log('nodeDidChange');
-    },
-    'graphDidUpdate': function(event, template){
-        console.log('nodeDidChange');
-    }
-});
-
 // Template => fluxo
 Template.fluxo.helpers({
     name: function(){
@@ -124,7 +145,27 @@ Template.fluxo.events({
         /** @type Graph **/
         const graph = window.graph;
         var elements = graph.cy.elements().jsons();
-        Meteor.call('saveGraph', this.id_tipo_doc, elements);
+        // Meteor.call('saveGraph', this.id_tipo_doc, elements);
+        alert("Desabilitado: Nada acontece");
     }
 });
 
+Template.graphControlls.helpers({
+    layouts: function(){
+        // Todo: Tem que ter forma mais bonita de fazer isso... https://github.com/aldeed/meteor-simple-schema/issues/242
+        const layouts = CyGraphs.simpleSchema()._schema.layout.allowedValues;
+        
+        return layouts.map(function(l){ return {name: l} });
+    }
+});
+
+Template.graphControlls.events({
+    'click a': function(event, template){
+        event.preventDefault();
+        const layout = this.name;
+        /** @type Graph **/
+        const graph = window.graph;
+        // Meteor.call('changeGraphLayout', graph.id, layout);
+        graph.changeLayout(layout);
+    }
+});
