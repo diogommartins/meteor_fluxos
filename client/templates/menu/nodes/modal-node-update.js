4940332b373e867a858164f8cc9852efaa69cb44
diogommartins <diogo.martins@unirio.br>
2016-03-31 12:14:48 -0300

    Adicionado autosave de forms de atualização

--- client/templates/menu/nodes/modal-node-update.js
+++ client/templates/menu/nodes/modal-node-update.js
@@ -2,6 +2,9 @@
  * Created by diogomartins on 3/30/16.
  */
 Template.updateNode.helpers({
+    autoSaveMode: function(){
+        return true;
+    },
     node: function(){
         const node = window.graph.nodesMenu.currentItem;
         return Nodes.findOne(node._id);
