d06dda1087b15b7b00dfd2db002f73029632691b
diogommartins <diogo.martins@unirio.br>
2016-03-31 12:55:53 -0300

    Refatoração na estrutura do projeto

--- client/controllers/fluxo.js
+++ client/controllers/fluxo.js
@@ -81,22 +81,5 @@ Template.fluxo.events({
     }
 });
 
-Template.graphControlls.helpers({
-    layouts: function(){
-        // Todo: Tem que ter forma mais bonita de fazer isso... https://github.com/aldeed/meteor-simple-schema/issues/242
-        const layouts = CyGraphs.simpleSchema()._schema.layout.allowedValues;
-        
-        return layouts.map(function(l){ return {name: l} });
-    }
-});
 
-Template.graphControlls.events({
-    'click a': function(event, template){
-        event.preventDefault();
-        const layout = this.name;
-        /** @type Graph **/
-        const graph = window.graph;
-        // Meteor.call('changeGraphLayout', graph.id, layout);
-        graph.changeLayout(layout);
-    }
-});
+
---
+++ client/templates/controlls.js
@@ -0,0 +1,42 @@
+/**
+ * Created by diogomartins on 3/31/16.
+ */
+Template.graphControlls.helpers({
+    layouts: function(){
+        // Todo: Tem que ter forma mais bonita de fazer isso... https://github.com/aldeed/meteor-simple-schema/issues/242
+        const layouts = CyGraphs.simpleSchema()._schema.layout.allowedValues;
+
+        return layouts.map(function(l){ return {name: l} });
+    }
+});
+
+Template.graphControlls.events({
+    'click a': function(event, template){
+        event.preventDefault();
+        const layout = this.name;
+        /** @type Graph **/
+        const graph = window.graph;
+        // Meteor.call('changeGraphLayout', graph.id, layout);
+        graph.changeLayout(layout);
+    }
+});
+
+/**
+ *  Autosave Toggler
+ */
+
+Template.autoSaveButton.helpers({
+    'btnClass': function(){
+        return Session.get('autoSaveMode') ? 'btn-success' : 'btn-danger';
+    },
+    'btnContent': function(){
+        return Session.get('autoSaveMode') ? 'Autosave ligado' : 'Autosave desligado';
+    }
+});
+
+Template.autoSaveButton.events({
+    'click': function(){
+        Session.set('autoSaveMode', !Session.get('autoSaveMode'));
+        console.log("Toggling autosave ", Session.get('autoSaveMode'));
+    }
+});
