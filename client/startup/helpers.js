f85ec7f0504d1bef04ac941cd4a1dbc42f2edfa7
diogommartins <diogo.martins@unirio.br>
2016-04-04 20:35:00 -0300

    Adicionado helper para logo

---
+++ client/startup/helpers.js
@@ -0,0 +1,6 @@
+/**
+ * Created by diogomartins on 4/4/16.
+ */
+Template.registerHelper("logoURL", function(){
+    return "http://sistemas.unirio.br/projetos_ensino/static/images/layout2/logo_dtic_final.png";     
+});
