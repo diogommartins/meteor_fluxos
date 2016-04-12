ae6d922a111b3c406164eb138a4dacf84548af6e
diogommartins <diogo.martins@unirio.br>
2016-04-12 17:49:42 -0300

    Adicionado scrolltop e scrollbottom e pequenas refatorações

--- client/controllers/tramitacoes.js
+++ client/controllers/tramitacoes.js
@@ -18,22 +18,31 @@ Template.tramitacoes.helpers({
 
 Template.tramitacoes.events({
     'click .play-animation': function(event, template){
+        /** @type: Graph **/
         const graph = window.graph;
-        const steps = template.data.tramitacoes.fetch();
         const $btn = $(event.currentTarget).button('loading');
 
-        const animation = template.data.animation = new GraphAnimation(graph, steps);
-
-        animation.playAnimation(1000, ()=>{
+        graph.plugins.animation.playAnimation(1000, ()=>{
             $btn.button('reset');
-            template.data.animation = null;
         });
     },
     'click .stop-animation': function(event, template){
+        /** @type: Graph **/
+        const graph = window.graph;
         /** @type: GraphAnimation **/
-        const animation = template.data.animation;
-        if (animation)
+        const animation = graph.plugins.animation;
+
+        if (typeof animation !== "undefined")
             animation.stopAnimation();
+        else{
+            alert("Sem animação");
+        }
+    },
+    'click .scroll-top': function(){
+        $('ul.timeline > li').first().scrollintoview({ duration: "slow" });
+    },
+    'click .scroll-bottom': function(){
+        $('ul.timeline > li').last().scrollintoview({ duration: "slow" })
     },
     'graph.didRender': function(event, template, graph){
         this.isGraphRendered.set(true);
@@ -46,8 +55,8 @@ Template.tramitacoes.events({
     'graph.willAnimateElement': function(event, template, step, $elem){
         this.currentSequencia.set(step.SEQUENCIA);
         this.$slider.slider('value', step.SEQUENCIA);
-
-        $('ul.timeline').find('li.active-item').next().scrollintoview({ duration: "normal" });
+        const timelineActiveItem = $('ul.timeline').find('li.active-item');
+        timelineActiveItem.next().scrollintoview({ duration: "normal" });
         
     },
     'graph.didAnimateElement': function(event, template, step, $elem){
@@ -59,7 +68,7 @@ Template.tramitacoes.events({
 
 Template.tramitacoes.onCreated(function(){
     const id_documento = this.data.id_documento;
-    this.data.tramitacoes = Tramitacoes.find({ ID_DOCUMENTO: id_documento });
+    var tramitacoes = this.data.tramitacoes = Tramitacoes.find({ ID_DOCUMENTO: id_documento });
     this.data.currentSequencia = new ReactiveVar(1);
     this.data.isGraphRendered = new ReactiveVar(false);
     
@@ -68,6 +77,7 @@ Template.tramitacoes.onCreated(function(){
             const graph = new Graph(id_tipo_doc).renderGraph();
 
             graph.cy.ready(function(){
+                graph.registerPlugin('animation', new GraphAnimation(graph, tramitacoes.fetch()));
                 const layout = graph.collection.fetch()[0].layout;
                 graph.load(elements).applyStyle(layout);
             });
@@ -78,8 +88,26 @@ Template.tramitacoes.onCreated(function(){
 
 Template.tramitacoesSlider.events({
     'slider.userslide': function(event, template){
-        template.data.currentSequencia.set(event.ui.value);
-        $('ul.timeline').find('li.active-item').scrollintoview({ duration: "fast" });
+        const sequencia = event.ui.value;
+
+        // Atualiza sequencia
+        template.data.currentSequencia.set(sequencia);
+
+        // Rola timeline
+        const timelineItem = $('ul.timeline').find('li.active-item');
+        timelineItem.scrollintoview({ duration: "fast" });
+
+        // Marca item corrente no grafo e para animação, se houver
+        /** @type: Graph **/
+        const graph = window.graph;
+        /** @type: GraphAnimation **/
+        const animation = graph.plugins.animation;
+        const tramitacao = animation.getStepFromSequencia(sequencia);
+
+        const $elem = graph.cy.getElementById(tramitacao.elementId);
+
+        animation.stopAnimation().clearHighlightedElements();
+        GraphAnimation.highlightElement($elem);
     }
 });
 
