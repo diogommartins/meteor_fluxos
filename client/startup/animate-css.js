af5591b861dc3ecc40e5a9950051c19a52e73487
diogommartins <diogo.martins@unirio.br>
2016-04-12 18:32:01 -0300

    Refatoração estética e adição de $.animateCss

---
+++ client/startup/animate-css.js
@@ -0,0 +1,11 @@
+/**
+ * Created by diogomartins on 4/12/16.
+ */
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
