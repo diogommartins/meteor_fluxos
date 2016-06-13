3e1762e659d9ea164f6f63308721849e19d347f1
diogommartins <diogo.martins@unirio.br>
2016-06-13 19:43:29 -0300

    Adicionado serializavel de grafo mock

--- client/controllers/mock_graph.js
+++ client/controllers/mock_graph.js
@@ -1,66 +1,353 @@
-/**
- * Created by diogomartins on 6/13/16.
- */
-MOCK_CROMATICO =
-    {
-    name: "AA",
-    nodes: [
+TEST_MOCK_GRAPH = {
+    "elements": {
+        "nodes": [
+            {
+                "data": {
+                    "id": "1465851200550",
+                    "id_tipo_doc": 999,
+                    "name": "a",
+                    "color": "rgb(100,100,100)"
+                },
+                "position": {
+                    "x": 173.5,
+                    "y": 128
+                },
+                "group": "nodes",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "id": "1465852816659",
+                    "id_tipo_doc": 999,
+                    "name": "d",
+                    "color": "rgb(100,100,100)"
+                },
+                "position": {
+                    "x": 804.9294,
+                    "y": 348
+                },
+                "group": "nodes",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "id": "1465852818740",
+                    "id_tipo_doc": 999,
+                    "name": "e",
+                    "color": "rgb(100,100,100)"
+                },
+                "position": {
+                    "x": 770.9294,
+                    "y": 563
+                },
+                "group": "nodes",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "id": "1465852826245",
+                    "id_tipo_doc": 999,
+                    "name": "f",
+                    "color": "rgb(100,100,100)"
+                },
+                "position": {
+                    "x": 967.5,
+                    "y": 111
+                },
+                "group": "nodes",
+                "removed": false,
+                "selected": true,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "id": "1465852828403",
+                    "id_tipo_doc": 999,
+                    "name": "c",
+                    "color": "rgb(100,100,100)"
+                },
+                "position": {
+                    "x": 325.0706,
+                    "y": 547
+                },
+                "group": "nodes",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "id": "1465852900156",
+                    "id_tipo_doc": 999,
+                    "name": "b",
+                    "color": "rgb(100,100,100)"
+                },
+                "position": {
+                    "x": 519.0706,
+                    "y": 250
+                },
+                "group": "nodes",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            }
+        ],
+        "edges": [
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465852826245",
+                    "target": "1465852818740",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853776636"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465852900156",
+                    "target": "1465852816659",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853664123"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465852816659",
+                    "target": "1465852818740",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853679088"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465852818740",
+                    "target": "1465852828403",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853689473"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465852900156",
+                    "target": "1465852818740",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853696187"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465852900156",
+                    "target": "1465852828403",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853703038"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465851200550",
+                    "target": "1465852818740",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853711061"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465851200550",
+                    "target": "1465852900156",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853720555"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465852826245",
+                    "target": "1465851200550",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853753764"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            },
+            {
+                "data": {
+                    "name": "Nada",
+                    "source": "1465852826245",
+                    "target": "1465852900156",
+                    "id_tipo_doc": 999,
+                    "id": "temp_1465853760447"
+                },
+                "position": {},
+                "group": "edges",
+                "removed": false,
+                "selected": false,
+                "selectable": true,
+                "locked": false,
+                "grabbable": true,
+                "classes": ""
+            }
+        ]
+    },
+    "style": [
         {
-            group: 'nodes',
-            data: {
-                id: 'node_a',
-                name: 'a',
-                id_tipo_doc: 999
+            "selector": "node",
+            "style": {
+                "text-valign": "center",
+                "text-halign": "center",
+                "height": "2em",
+                "width": "2em",
+                "background-color": "data(color)",
+                "label": "data(name)"
             }
         },
         {
-            group: 'nodes',
-            data: {
-                id: 'node_b',
-                name: 'b',
-                id_tipo_doc: 999
+            "selector": "edge",
+            "style": {
+                "width": "2px",
+                "line-color": "#777"
             }
         },
         {
-            group: 'nodes',
-            data: {
-                id: 'node_c',
-                name: 'c',
-                id_tipo_doc: 999
+            "selector": ":selected",
+            "style": {
+                "text-outline-color": "#fff",
+                "text-outline-width": "2px",
+                "line-color": "#61bffc",
+                "target-arrow-color": "#61bffc",
+                "label": "data(name)"
             }
         },
         {
-            group: 'nodes',
-            data: {
-                id: 'node_d',
-                name: 'd',
-                id_tipo_doc: 999
+            "selector": ".highlighted",
+            "style": {
+                "background-color": "#2e6da4",
+                "line-color": "#2e6da4",
+                "target-arrow-color": "#2e6da4"
             }
         },
         {
-            group: 'nodes',
-            data: {
-                id: 'node_e',
-                name: 'e',
-                id_tipo_doc: 999
-            }
-        },
-        {
-            group: 'nodes',
-            data: {
-                id: 'node_f',
-                name: 'f',
-                id_tipo_doc: 999
+            "selector": ".walkedby",
+            "style": {
+                "background-color": "#3f903f",
+                "line-color": "#3f903f",
+                "target-arrow-color": "#3f903f"
             }
         }
     ],
-    edges: [
-        {
-            data: {
-                source: 'node_a',
-                target: 'node_b'
-            },
-            group: 'edges'
-        }
-    ]
+    "zoomingEnabled": false,
+    "userZoomingEnabled": true,
+    "zoom": 1,
+    "minZoom": 1.0E-50,
+    "maxZoom": 1.0E50,
+    "panningEnabled": true,
+    "userPanningEnabled": true,
+    "pan": {
+        "x": 0,
+        "y": 0
+    },
+    "boxSelectionEnabled": true,
+    "renderer": {
+        "name": "canvas"
+    }
 };
